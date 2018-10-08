10a5b7ffa0034840e8df166c871e5864650f0b90
DeejayRingis <djringis@gmail.com>
2018-10-08 17:28:16 +0100

    Clean vpx 8 oct 18

---
+++ vp8/common/treecoder.c.d
@@ -0,0 +1,17 @@
+vp8/common/treecoder.c.o vp8/common/treecoder.c.d: vp8/common/treecoder.c /usr/include/stdc-predef.h \
+ /usr/include/assert.h /usr/include/features.h \
+ /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h /usr/include/stdio.h \
+ /usr/lib/gcc/x86_64-linux-gnu/5/include/stddef.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h /usr/include/libio.h \
+ /usr/include/_G_config.h /usr/include/wchar.h \
+ /usr/lib/gcc/x86_64-linux-gnu/5/include/stdarg.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/sys_errlist.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio.h vp8/common/treecoder.h \
+ vpx/vpx_integer.h /usr/lib/gcc/x86_64-linux-gnu/5/include/stdint.h \
+ /usr/include/stdint.h /usr/include/x86_64-linux-gnu/bits/wchar.h \
+ /usr/include/inttypes.h
---
+++ vpx_dsp/x86/highbd_variance_sse2.c.d
@@ -0,0 +1,9 @@
+vpx_dsp/x86/highbd_variance_sse2.c.o vpx_dsp/x86/highbd_variance_sse2.c.d: vpx_dsp/x86/highbd_variance_sse2.c \
+ /usr/include/stdc-predef.h vpx_config.h vpx_ports/mem.h vpx_config.h \
+ vpx/vpx_integer.h /usr/lib/gcc/x86_64-linux-gnu/5/include/stddef.h \
+ /usr/lib/gcc/x86_64-linux-gnu/5/include/stdint.h /usr/include/stdint.h \
+ /usr/include/features.h /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/include/x86_64-linux-gnu/bits/wchar.h /usr/include/inttypes.h
